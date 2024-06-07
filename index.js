85bf9235c7c7f72ad7e89d2db22702429c1d2afa
diegodev37 <diego.dev.37@gmail.com>
2024-06-07 19:46:45 -0300

    Aula de atualização de usuarios.

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-const express = require("express")
-const uuid = require("uuid")
+const express = require('express')
+const uuid = require('uuid')
 
 const port = 3000
 const app = express()
+app.use(express.json())
 
 const users = []
 
 app.get('/users', (request, response) => {
     return response.json(users)
-
 })
 
 app.post('/users', (request, response) => {
 
-    const{ name, age } = request.body
-    
+    const { name, age } = request.body
+
     const user = { id: uuid.v4(), name, age }
 
     users.push(user)
 
-    return response.status(201).json(users)
+    return response.status(201).json(user)
+
 
 })
+
+app.put('/users/:id', (request, response) => {
+    const{ id } = request.params
+    const { name, age } = request.body 
+    
+    const updatedUser = { id, name, age }
+
+    const index = users.findIndex( user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({ menssage:"User not found"})
+    }
+
+    users[index] = updatedUser
+
+
+    
+    return response.json(updatedUser)
+})
+
 
 
 app.listen(port, () => {
